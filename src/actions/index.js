13cd56cb0687c956fff916aba1c53e00121ebd1f
miketheo423 <miketheo423@gmail.com>
2018-01-28 19:55:04 -0700

    Adds switch statement for check items and void item function

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ export const FETCH_CHECKS = 'FETCH_CHECKS';
 export const FETCH_ONE_CHECK = 'FETCH_ONE_CHECK';
 export const FETCH_MENU_ITEMS = 'FETCH_MENU_ITEMS';
 export const ADD_ITEM = 'ADD_ITEM';
+export const VOID_ITEM = 'VOID_ITEM';
 
 const ROOT_URL = 'https://check-api.herokuapp.com';
 const AUTH_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI3M2I1MzJiLWJlZWItNDExYi1hMTg5LWJjZmJlNWVlYzM5MCIsIm5hbWUiOiJqdW5pb3IgIzgifQ.FOOXhSHXPe3oJ0gs_eCJkZK67N5OEEqrG-IMDxBvZ8w';
@@ -97,12 +98,26 @@ export function addItem(checkId, itemId) {
     data: {
       itemId: itemId
     }
-  })
-  // .then(response => {
-  //   console.log(response);
-  // }).catch(error => {
-  //   console.log(error.response);
-  // });
+  });
+
+  return {
+    type: ADD_ITEM,
+    payload: request
+  }
+}
+
+export function voidItem(itemId) {
+  const request = axios({
+    url: `${ROOT_URL}/checks/${checkId}/voidItem`,
+    method: 'put',
+    headers: { 
+      'Authorization' : AUTH_KEY,
+      'Content-Type': 'application/json' 
+    },
+    data: {
+      orderedItemId: itemId
+    }
+  });
 
   return {
     type: ADD_ITEM,
--- src/components/Table_Show.js
+++ src/components/Table_Show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { createCheck, fetchChecks, fetchMenuItems, fetchOneCheck, addItem } from '../actions';
+import { createCheck, fetchChecks, fetchMenuItems, fetchOneCheck, addItem, voidItem } from '../actions';
 
 
 class TableShow extends Component {
@@ -40,19 +40,73 @@ class TableShow extends Component {
 
   addItem(checkId, itemId) {
     this.props.addItem(checkId, itemId);
+    this.props.fetchOneCheck(checkId);
   }
 
   renderCheck() {
     return _.map(this.props.oneCheck.orderedItems, checkItem => {
-      return (
-        <div key={checkItem.id} className="check-item list-group-item">
-          <span>{checkItem.id}</span>
-          <div className="text-xs-right">
-            <button className="btn btn-danger">Void</button>
+      let itemName = '';
+      let price = '';
+      let checkItemName = checkItem.itemId;
+        switch(checkItemName) {
+          case "348e706c-ab3b-4a6e-a391-8de96ac7e0a3":
+            itemName = "PULL-APART BREAD";
+            price = 4.5;
+            break;
+          case "92d26789-a296-4910-b7a9-b08e68d9e44d":
+            itemName = "GREEN SALAD";
+            price = 8;
+            break;
+          case "abae32ec-05e5-4072-ba54-3a46764a5eff":
+            itemName = "MORTGAGE LIFTER BEANS";
+            price = 6;
+            break;
+          case "242b1e7c-c233-4324-8b8c-cbf43723395b":
+            itemName = "TOMATO TOAST";
+            price = 5.5;
+            break;
+          case "35b6c0b0-afdc-4df6-a690-30fbcd4a2a04":
+            itemName = "MORGANE’S BEEF CHILI";
+            price = 6;
+            break;
+          case "3aa7eef8-a37a-4a05-83c5-22e99e531781":
+            itemName = "CHICKEN CAESAR";
+            price = 13;
+            break;
+          case "7fde8abf-0589-4446-9905-9185b4c2b598":
+            itemName = "THREE SISTERS SALAD";
+            price = 13;
+            break;
+          case "cf09ccf2-ece2-4771-81c5-deff1fe08d79":
+            itemName = "MARINATED STEAK BOWL";
+            price = 15;
+            break;
+          case "a34074ea-2949-40f9-94fe-d3404607861b":
+            itemName = "OUR BURGER";
+            price = 14;
+            break;
+          case "4b48707c-619b-42b1-8b8a-3da51feacf95":
+            itemName = "HAM & CHEESE PRESS";
+            price = 12.5;
+            break;
+          case "6b6e59e3-0861-48b1-85c0-778df1126e19":
+            itemName = "PATTY MELT";
+            price = 12.5;
+            break;
+          case "f58948c7-9feb-44ea-b0cc-9013406b9a51":
+            itemName = "COOKIES & MILK";
+            price = 5;
+            break;
+        }
+        return (
+          <div key={checkItem.id} className="check-item list-group-item">
+            <span>{itemName} / {price}</span>
+            <div className="text-xs-right">
+              <button className="btn btn-danger">Void</button>
+            </div>
           </div>
-        </div>
-      )
-    });
+        )
+      });
   }
 
   renderMenu() {
@@ -103,4 +157,4 @@ function mapStateToProps({ tables, checks, menu, oneCheck}, ownProps) {
   }
 }
 
-export default connect(mapStateToProps, { createCheck, fetchMenuItems, fetchOneCheck, addItem, fetchChecks })(TableShow);
+export default connect(mapStateToProps, { createCheck, fetchMenuItems, fetchOneCheck, addItem, fetchChecks, voidItem })(TableShow);
